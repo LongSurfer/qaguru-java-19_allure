74924a1c14582028deb773f1bf1ae1ce536561bc
LongSurfer <ivanov2avi@yandex.ru>
2023-09-09 19:23:09 +0300

    add simple test

--- src/test/java/guru/qa/tests/IssueTest.java
+++ src/test/java/guru/qa/tests/IssueTest.java
@@ -1,11 +1,11 @@
 package guru.qa.tests;
 
-import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.logevents.SelenideLogger;
 import guru.qa.TestBase;
 import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.Test;
 
+import static com.codeborne.selenide.Condition.exist;
 import static com.codeborne.selenide.Selectors.withText;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
@@ -15,12 +15,13 @@ public class IssueTest extends TestBase {
     @Test
     void issueTest() {
         SelenideLogger.addListener("allure", new AllureSelenide());
-        open("");
-        $(".header-search-input").click();
-        $(".header-search-input").setValue("eroshenkoam/allure-example").pressEnter();
 
-        $(linkText("eroshenkoam/allure-example")).click();
+        open("");
+        $("[data-target=\"qbsearch-input.inputButtonText\"]").click();
+        $("#query-builder-test").setValue("egolikov/allure-reports-21").pressEnter();
+
+        $(linkText("egolikov/allure-reports-21")).click();
         $("#issues-tab").click();
-        $(withText("#80")).should(Condition.exist);
+        $(withText("Hello! This is test issue")).should(exist);
     }
 }
