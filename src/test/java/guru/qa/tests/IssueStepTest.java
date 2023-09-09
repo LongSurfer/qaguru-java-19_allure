5e3d8307106f6a51bb21bdca78179f6262c60931
LongSurfer <ivanov2avi@yandex.ru>
2023-09-09 19:23:58 +0300

    add step tests

--- src/test/java/guru/qa/tests/IssueStepTest.java
+++ src/test/java/guru/qa/tests/IssueStepTest.java
@@ -1,11 +1,11 @@
 package guru.qa.tests;
 
-import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.logevents.SelenideLogger;
 import guru.qa.TestBase;
 import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.Test;
 
+import static com.codeborne.selenide.Condition.exist;
 import static com.codeborne.selenide.Selectors.withText;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
@@ -14,8 +14,8 @@ import static org.openqa.selenium.By.linkText;
 
 public class IssueStepTest extends TestBase {
 
-    public static final String REPOSITORY = "eroshenkoam/allure-example";
-    public static final int ISSUE = 80;
+    public static final String REPOSITORY = "egolikov/allure-reports-21";
+    public static final String ISSUE = "Hello! This is test issue";
     @Test
     void issueTest() {
         SelenideLogger.addListener("allure", new AllureSelenide());
@@ -25,8 +25,8 @@ public class IssueStepTest extends TestBase {
         });
 
         step("Search repository " + REPOSITORY, () -> {
-            $(".header-search-input").click();
-            $(".header-search-input").setValue(REPOSITORY).pressEnter();
+            $("[data-target=\"qbsearch-input.inputButtonText\"]").click();
+            $("#query-builder-test").setValue(REPOSITORY).pressEnter();
         });
 
         step("Click on repository link " + REPOSITORY, () -> {
@@ -38,8 +38,19 @@ public class IssueStepTest extends TestBase {
         });
 
         step("Check issue " + ISSUE, () -> {
-             $(withText("#" + ISSUE)).should(Condition.exist);
+             $(withText(ISSUE)).should(exist);
         });
 
+    }
+
+    @Test
+    void issueWebStepsTest() {
+        WebStep steps = new WebStep();
+
+        steps.openMainPage();
+        steps.searchForRepository(REPOSITORY);
+        steps.clickOnRepositoryLink(REPOSITORY);
+        steps.clickOnIssueTab();
+        steps.checkIssue(ISSUE);
     }
 }
